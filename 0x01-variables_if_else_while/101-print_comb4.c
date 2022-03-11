7833c8644b92005d3c54a84ed7ed21ec6f7b5e0e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-03-11 01:00:45 -0800

    Add C program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all possible combinations
+ * of double-digit numbers from the ANSI character set.
+ * Return: Always 0
+ */
+int main(void)
+{
+int digit_one;
+int digit_two;
+int digit_three;
+
+digit_one = 47;
+
+while (digit_one < 57)
+{
+digit_one++;
+digit_two = 47;
+
+while (digit_two < 57)
+{
+digit_two++;
+digit_three = 47;
+
+while (digit_three < 57)
+{
+digit_three++;
+
+if (digit_one == digit_two || digit_one == digit_three ||
+digit_two == digit_three || digit_two <= digit_one || digit_three <= digit_two)
+continue;
+
+putchar(digit_one);
+putchar(digit_two);
+putchar(digit_three);
+
+if (digit_one == 55 && digit_two == 56 & digit_three == 57)
+break;
+
+putchar(44);
+putchar(32);
+}
+}
+
+}
+
+putchar('\n');
+return (0);
+}
