4b477b686c84dda1dbe42598316869a4884b5cb1
MosesSoftEng <moses.soft.eng@gmail.com>
2022-03-11 04:24:16 -0800

    Add corrections to code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,44 +8,41 @@
  */
 int main(void)
 {
-    int digit_one;
-    int digit_two;
-    int digit_three;
+	int digit_one, digit_two, digit_three;
 
-    digit_one = 47;
+	digit_one = 47;
+	while (digit_one < 57)
+	{
+		digit_one++;
+		digit_two = 47;
 
-    while (digit_one < 57)
-    {
-        digit_one++;
-        digit_two = 47;
-
-        while (digit_two < 57)
-        {
+		while (digit_two < 57)
+		{
 			digit_two++;
-            digit_three = 47;
+			digit_three = 47;
 
-            while (digit_three < 57)
-            {
-                digit_three++;
+			while (digit_three < 57)
+			{
+				digit_three++;
 
-                if (digit_one == digit_two || digit_one == digit_three ||
-                    digit_two == digit_three || digit_two <= digit_one || digit_three <= digit_two)
-                    continue;
+				if (digit_one == digit_two || digit_one == digit_three ||
+					digit_two == digit_three || digit_two <= digit_one ||
+					digit_three <= digit_two)
+					continue;
 
-                putchar(digit_one);
-                putchar(digit_two);
-                putchar(digit_three);
+				putchar(digit_one);
+				putchar(digit_two);
+				putchar(digit_three);
 
-                if (digit_one == 55 && digit_two == 56 && digit_three == 57)
-                    break;
+				if (digit_one == 55 && digit_two == 56 && digit_three == 57)
+					break;
 
-                putchar(44);
-                putchar(32);
-            }
-        }
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
 
-    }
-
-    putchar('\n');
-    return (0);
+	putchar('\n');
+	return (0);
 }
