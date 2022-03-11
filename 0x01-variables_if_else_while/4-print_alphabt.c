017992fae9691397a3afbcb7ee0f92481e53c3dc
MosesSoftEng <moses.soft.eng@gmail.com>
2022-03-10 23:02:14 -0800

    Add C program that printsthe alphabet in lowercase, followed by a new line except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program that prints the alphabet in lowercase,
+ * and uppercase followed by a new line. Prints alphabets from
+ * the ANSI character set.
+ * Return: Always 0
+ */
+int main(void)
+{
+    int ansi_number_lowercase;
+
+    ansi_number_lowercase = 97;
+
+    while (ansi_number_lowercase < 123)
+    {
+        if(ansi_number_lowercase == 'e' || ansi_number_lowercase == 'q') {
+            ansi_number_lowercase++;
+            continue;
+        }
+        putchar(ansi_number_lowercase);
+        ansi_number_lowercase++;
+    }
+
+    putchar('\n');
+    return (0);
+}
