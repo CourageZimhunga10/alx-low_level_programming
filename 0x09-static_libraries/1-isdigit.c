b587ba0ed79d66a63767f02df5c9a35719209d63
MosesSoftEng <moses.soft.eng@gmail.com>
2022-04-01 14:49:49 +0300

    Create the static library libmy.a

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * _isdigit - Checks for a digit (0-9).
  * @c: The number to be checked.
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,3 @@
-#include "main.h"
-
 /**
  * _puts - prints passed string to stdout.
  *
@@ -7,14 +5,4 @@
  */
 void _puts(char *str)
 {
-	int i = 0;
-	char c = str[0];
-
-	while (c != '\0')
-	{
-		_putchar(c);
-		i++;
-		c = str[i];
-	}
-	_putchar('\n');
 }
