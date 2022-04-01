158af565ebd7e302a33c740e37ccee052bb942a0
MosesSoftEng <moses.soft.eng@gmail.com>
2022-04-01 15:15:48 +0300

    Create the static library libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _puts - prints passed string to stdout.
  *
@@ -5,4 +7,14 @@
  */
 void _puts(char *str)
 {
+	int i = 0;
+	char c = str[0];
+
+	while (c != '\0')
+	{
+		_putchar(c);
+		i++;
+		c = str[i];
+	}
+	_putchar('\n');
 }
