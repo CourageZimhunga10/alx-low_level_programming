e1989086614f59ed459efae305d1c75cc4fb05b8
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-04-24 15:53:58 +0300

    Update documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
-  * main - Program entry point
+  * main - C program that prints a string.
   *
+  * Description: C program that prints exactly "Programming is like building a
+  * multilingual puzzle, followed by a new line.
   * Return: 0 Always
   */
 int main(void)
