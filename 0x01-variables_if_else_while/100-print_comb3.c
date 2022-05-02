a30d10f207a9eec6725b073c4607f2330ff2c88d
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-02 12:21:34 +0300

    Update code logic

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Program that prints all possible different combinations of two
+ * digits
  *
  * Description: program that prints all possible combinations
  * of double-digit numbers from the ANSI character set.
@@ -8,32 +9,42 @@
  */
 int main(void)
 {
-	int digit_one;
+	int digit_one = '0';
 	int digit_two;
 
-	digit_one = 47;
-
-	while (digit_one < 57)
+	while (digit_one <= '9') /* Parent loop for first digit */
 	{
-		digit_one++;
-		digit_two = 47;
-
-		while (digit_two < 57)
+		digit_two = '0';
+		while (digit_two <= '9') /* Child loop for second digit */
 		{
-			digit_two++;
-
-			if (digit_one == digit_two || digit_two <= digit_one)
+			/* Prevent repeating combinations e.g 01 and 10 */
+			if (digit_one > digit_two)
+			{
+				digit_two++;
 				continue;
+			}
+
+			/* Prevent sawme digit */
+			if (digit_one == digit_two)
+			{
+				digit_two++;
+				continue;
+			}
 
 			putchar(digit_one);
 			putchar(digit_two);
 
-			if (digit_one == 56 && digit_two == 57)
-				break;
+			/* Prevent output if at the end of combination i.e 89*/
+			if (digit_one != '8' || digit_two != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
 
-			putchar(44);
-			putchar(32);
+			digit_two++;
 		}
+
+		digit_one++;
 	}
 
 	putchar('\n');
