2579900395595d088573fc730c2d05847ada469f
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-02 12:35:38 +0300

    Improve commenting

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,46 +1,57 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Program that prints combinations of three digits
  *
- * Description: program that prints all possible combinations
+ * Description: Program that prints all possible combinations
  * of double-digit numbers from the ANSI character set.
+ *
  * Return: Always 0
  */
 int main(void)
 {
-	int digit_one, digit_two, digit_three;
+	int digit_one = '0';
+	int digit_two, digit_three;
 
-	digit_one = 47;
-	while (digit_one < 57)
+	while (digit_one <= '9') /* Parent loop for first digit */
 	{
-		digit_one++;
-		digit_two = 47;
-
-		while (digit_two < 57)
+		digit_two = '0';
+		while (digit_two <= '9') /* Child loop for second digit */
 		{
-			digit_two++;
-			digit_three = 47;
-
-			while (digit_three < 57)
+			digit_three = '0';
+			while (digit_three <= '9') /* Child loop for second digit */
 			{
-				digit_three++;
-
-				if (digit_one == digit_two || digit_one == digit_three ||
-					digit_two == digit_three || digit_two <= digit_one ||
-					digit_three <= digit_two)
+				/* Prevent repeating combinations e.g 001, 010, 100 */
+				if (digit_one > digit_two || digit_two > digit_three)
+				{
+					digit_three++;
 					continue;
+				}
+
+				/* Prevent sawme digit */
+				if (digit_one == digit_two || digit_two == digit_three)
+				{
+					digit_three++;
+					continue;
+				}
 
 				putchar(digit_one);
 				putchar(digit_two);
 				putchar(digit_three);
 
-				if (digit_one == 55 && digit_two == 56 && digit_three == 57)
-					break;
+				/* Prevent output if at the end of combination i.e 89*/
+				if (digit_one != '7' || digit_two != '8' || digit_three != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
 
-				putchar(44);
-				putchar(32);
+				digit_three++;
 			}
+
+			digit_two++;
 		}
+
+		digit_one++;
 	}
 
 	putchar('\n');
