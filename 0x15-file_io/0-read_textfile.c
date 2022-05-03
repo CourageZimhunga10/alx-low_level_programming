7242871a872971f01f335c0b5f26deb78d519a54
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-03 11:58:03 +0300

    Add function description

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file and prints it
+ * @filename: The pathname of file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: Number of letters it could read and print, otherwise 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t letters_printed_count = 0;
+
+	return (letters_printed_count);
+}
