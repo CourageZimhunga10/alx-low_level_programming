4d5e13b19966cfd4bf051d7d0a18c95e9490b8f0
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-03 11:25:56 +0300

    Update 0-binary_to_uint.c

--- alx-low_level_programming_practise/0x14-bit_manipulation/0-binary_to_uint.c
+++ alx-low_level_programming_practise/0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,49 +1,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - Gets length of passed string
- *
- * @s: Pointer to a String
- *
- * Return: The length of @s
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-	char c = s[0];
-
-	while (c != '\0')
-	{
-		i++;
-		c = s[i];
-	}
-
-	return (i);
-}
-
-/**
- * _pow_recursion - Returns the value of x raised to the power of y.
- * @x: The number to be raised.
- * @y: The power.
- *
- * Return: The value of x raised to the power of y.
- */
-int _pow_recursion(int x, int y)
-{
-	int result = x;
-
-	if (y < 0)
-		return (-1);
-
-	else if (y == 0)
-		return (1);
-
-	result *= _pow_recursion(x, y - 1);
-
-	return (result);
-}
-
-/**
  * binary_to_uint - a function that converts a binary number
  *                  to an unsigned int
  * @b: string of 0 and 1 chars.
@@ -52,33 +9,16 @@ int _pow_recursion(int x, int y)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
-	int power;
-	int b_index;
+	unsigned int decimal = 0;
 
-	sum = 0;
-	power = 0;
-	b_index = _strlen(b) - 1; /* Minus 1 to get the last index */
-
-	while(b_index > -1)/* Loop b string backwards */
+	while(*b)
 	{
-		char c;
-		int step;
-
-		c = *(b + b_index);
-
-		if(c == '0')
-			step = 0 * _pow_recursion(2, power);  /* Multiply */
-		else
-			step = 1 * _pow_recursion(2, power);
-		
-		sum += step;
-		
-		printf("b_index: %d, step: %d, power: %d\n", b_index, step, power);
-
-		power++;
-		b_index--;
+		if (*b != 48 && *b != 49 && b == NULL)
+			return 0;
+			
+		decimal = (decimal * 2) + (*b - 48);
+		b++;
 	}
-	
-	return (sum);
+
+	return(decimal);
 }
