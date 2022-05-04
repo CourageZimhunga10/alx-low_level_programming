06e85e18ef1ed26d8c89b78a5c24388d8654393b
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-04 17:57:46 +0300

    Update code logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 
 	/* Open file*/
 	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC,
-	0600);
+	S_IRUSR | S_IWUSR);
 
 	if (file_descriptor == -1)
 	{
