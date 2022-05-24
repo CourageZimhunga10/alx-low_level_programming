6a3dd66e3ffd75fcb8c5ed637bb34f42cdd40532
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-24 12:14:56 +0300

    Change code logic

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints lowercase the alphabet 10 times.
+ * print_alphabet_x10 - Prints lowercase alphabets 10 times
  */
 void print_alphabet_x10(void)
 {
-	int i;
+	int j;
 
-	for (i = 0; i < 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		int j;
+		int i = 'a';
 
-		for (j = 'a'; j <= 'z'; j++)
-		{
-			_putchar(j);
-		}
+		while (i < '{')
+			_putchar(i++);
 
 		_putchar('\n');
 	}
