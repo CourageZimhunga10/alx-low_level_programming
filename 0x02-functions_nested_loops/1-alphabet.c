2718712f61a9eaf3c6de914122328df95956efc3
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-24 11:54:48 +0300

    Change code logic

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,15 @@
 
 /**
  * print_alphabet - prints the alphabet, in lowercase
+ *
+ * return: nothing
  */
 void print_alphabet(void)
 {
-	int i;
+	int i = 'a';
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
+	while (i < '{')
+		_putchar(i++);
 
 	_putchar('\n');
 }
