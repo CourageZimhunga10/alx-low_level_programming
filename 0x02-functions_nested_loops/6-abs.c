cb1198269d4abbf29c7790414f27d82496d78b0c
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-24 12:50:43 +0300

    Improve code logic

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 /**
- * _abs - computes the absolute value of an integer.
- * @n: The integer to be computed.
+ * _abs - Computes the absolute value of an integer
+ * @n: The integer to compute absolute value
  *
- * Return:  Absolute value of the integer.
+ * Return:  Absolute value of @n
  */
 int _abs(int n)
 {
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+	if (n < 0)
+		n *= -1;
+
+	return (n);
 }
