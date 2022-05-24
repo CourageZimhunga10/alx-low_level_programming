6837d99472befe460d3f552dc17d3a7dca5df96f
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-24 12:31:06 +0300

    Improve code logic

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
 /**
- * _isalpha - checks if a character is alphabetic.
+ * _isalpha - Checks if a character is alphabetic
  * @c: The character to be checked.
  *
- * Return:  1 if character is alphabetic, 0 otherwise
+ * Return:  1 if @c is alphabetic, 0 otherwise
  */
 int _isalpha(int c)
 {
-	int r = 0;
+	int i = 0;
 
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		r = 1;
+	if ((c > '`' && c < '{') || (c > '@' && c < '['))
+		i = 1;
 
-	return  (r);
+	return  (i);
 }
