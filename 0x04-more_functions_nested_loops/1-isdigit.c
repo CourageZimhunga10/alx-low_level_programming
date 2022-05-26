f22fdc9cb731455ab3a9b433334582279775aefd
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-26 07:02:36 +0300

    Improve code logic

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _isdigit - Checks for a digit (0-9).
- * @c: The number to be checked.
+ * _isdigit - Checks for a digit
+ * @c: Number to be checked.
  *
- * Return: 1 if the number is a digit, 0 otherwise.
+ * Return: 1 if @c is a digit, 0 otherwise.
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c > '/' && c < ':')
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
