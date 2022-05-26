95a9a0ed28e3b357865e24895a96b706ca150963
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-26 07:21:24 +0300

    Improve code logic

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * print_numbers - Print the numbers from 0-9
  */
 void print_numbers(void)
 {
-	int i;
+	int i = '/';
 
-	for (i = 0; i < 10; i++)
-		_putchar(i + 48);
+	while (i++ < '9')
+		_putchar(i);
 
 	_putchar('\n');
 }
