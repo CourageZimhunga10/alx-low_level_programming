02242809c200036452edc7bd7e099bf0944f1626
MosesSoftEng <99524902+MosesSoftEng@users.noreply.github.com>
2022-05-26 09:15:20 +0300

    Improve code logic

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - Fizz-Buzz test.
+ * main - Fizz-Buzz test
  *
- * Description: Print the numbers from 1 to 100,
- * followed by a new line. But for multiples of
- * three print Fizz instead of the number and
- * for the multiples of five print Buzz. For
- * numbers which are multiples of both three
- * and five print FizzBuzz.
+ * Description: Print the numbers from 1 to 100, followed by a new line.
+ * But for multiples of three print Fizz instead of the number and for
+ * the multiples of five print Buzz. For numbers which are multiples of
+ * both three and five print FizzBuzz.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i < 101; i++)
+	while (i < 101)
 	{
+		/* Check this first */
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
@@ -27,7 +26,7 @@ int main(void)
 		else
 			printf("%d", i);
 
-		if (i < 100)
+		if (i++ < 100)
 			printf(" ");
 	}
 
