f3e5e1cca54de4bb267e4e3a236fdaf695ed4a1b
MosesSoftEng <moses.soft.eng@gmail.com>
2022-06-07 10:48:00 +0300

    backup

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,42 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer.
+ * print_number - Print an integer
  *
- * @n: The integer to be printed.
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int num, place_value = 1, reversed_num = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		n = -n;
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	/* Find highest place value of n*/
+	num = n;
+	while ((num /= 10) > 0)
+	{
+		place_value *= 10;
+	}
 
-	_putchar((num % 10) + '0');
+	/* Reverse the number */
+	num = n;
+	while (num > 0)
+	{
+		reversed_num += (num % 10) * place_value;
+		num /= 10;
+		place_value /= 10;
+	}
+
+	/* Print number run at least once*/
+	num = reversed_num;
+	do {
+		_putchar('0' + (num % 10));
+		num /= 10;
+	}
+	while ((num > 0))
+		;
 }
-
