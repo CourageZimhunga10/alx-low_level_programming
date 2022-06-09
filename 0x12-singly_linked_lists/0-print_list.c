9e5d7b070a17ef4879f743e604b01521026c55d9
MosesSoftEng <moses.soft.eng@gmail.com>
2022-06-09 07:57:41 +0000

    Improve code logic for Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - Prints all the elements of a list_t list
+ * @h: The list_t list
  *
- * Return: The number of nodes in h.
+ * Return: Number of nodes in @h
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
@@ -16,7 +15,6 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
