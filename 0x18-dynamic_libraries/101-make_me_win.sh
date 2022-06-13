c2c896afbe4024884f41657ab613a692093f710e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-06-13 14:40:11 +0000

    Attemp task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MosesSoftEng/alx-low_level_programming/tree/master/0x18-dynamic_libraries/injection.so
-export LD_PRELOAD=/tmp/injection.so
+wget -P /tmp https://github.com/MosesSoftEng/alx-low_level_programming/tree/master/0x18-dynamic_libraries/inject.so
+export LD_PRELOAD=/tmp/inject.so
