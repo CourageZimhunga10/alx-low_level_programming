ff786acf3ccbefd55be9e65200d2edf4a371effe
Moses Mwangi <ngigemoses1@outlook.com>
2022-07-01 16:43:19 +0300

    Task 1 attempt

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm
+ * @str: String to hash
+ *
+ * Return: Calculated hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash = 5381;
+	int c;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+
+	return (hash);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -7,5 +7,5 @@ betty hash_tables.h;
 betty 0-hash_table_create.c; gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-hash_table_create.c -o a; ./a
 
 ## 1-djb2.c
-betty 1-djb2.c
+betty 1-djb2.c; gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-djb2.c -o b; ./b
 
