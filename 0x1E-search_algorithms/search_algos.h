40a1ceea0e0e1b94ad25f21a539f08197126616e
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-05 11:02:01 +0300

    Create custom header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_ALGOS_HEADER
+#define SEARCH_ALGOS_HEADER
+/* Include guard to prevent multi inclusion error using #ifndef preprocessor */
+
+/* Libraries */
+/* Functions prototypes */
+#endif
