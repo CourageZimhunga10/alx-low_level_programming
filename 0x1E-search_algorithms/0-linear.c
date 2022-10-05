72d22fbebc557ed86686d214c01052167f6559c0
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-05 11:21:07 +0300

    Create task 0 file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search.h"
+
+/**
+ * linear_search - a function that searches for a value in an array of integers
+ *	using the Linear search algorithm
+ *
+ * @array: a pointer to the first element of the array
+ * @size:  is the number of elements in @array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located. otherwise -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	return (0);
+}
