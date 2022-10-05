870a4ac39141b050cc71678b0f0571bc21124e26
MosesSoftEng <moses.soft.eng@gmail.com>
2022-10-05 11:22:44 +0300

    Add function prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,9 @@
 /* Include guard to prevent multi inclusion error using #ifndef preprocessor */
 
 /* Libraries */
+    /* Library for size_t ssize_t */
+    #include <stdlib.h>
+
 /* Functions prototypes */
+	int linear_search(int *array, size_t size, int value);
 #endif
